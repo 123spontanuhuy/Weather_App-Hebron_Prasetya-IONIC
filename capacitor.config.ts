ac56acca8ed644e0c655b93790366aa125bf9de8
ebrone <hebronprasetya26@gmail.com>
2024-09-15 08:42:17 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Weather_App',
+  webDir: 'www'
+};
+
+export default config;
